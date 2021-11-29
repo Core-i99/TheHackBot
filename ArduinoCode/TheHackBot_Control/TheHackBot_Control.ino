e5a62065b8a7f134c0a8a29261e0df01c783ff61
TheHackGuy <stijnrombouts@outlook.com>
2021-11-29 21:33:09 +0100

    Add more buttons

--- ArduinoCode/TheHackBot_Control/TheHackBot_Control.ino
+++ ArduinoCode/TheHackBot_Control/TheHackBot_Control.ino
@@ -1,9 +1,9 @@
-#include <SoftwareSerial.h> 
+#include <SoftwareSerial.h>
 SoftwareSerial MyBTModule(3, 2); // RX & TX pins
 
 const int ledPin = 13; // Built in LED in Arduino board
 
-String msg,cmd;
+String msg, cmd;
 
 void setup() {
   // Initialization
@@ -15,23 +15,37 @@ void setup() {
 }
 
 void loop() {
-  
+
   // To read message received from other Bluetooth Device
-  if (MyBTModule.available() > 0){ // Check if there is data coming
+  if (MyBTModule.available() > 0) { // Check if there is data coming
     msg = MyBTModule.readString(); // Read the message as String
     Serial.println("Android Command: " + msg);
   }
 
   // Control LED in Arduino board
-  if (msg == "<turn on>"){
+  if (msg == "<turn on>") {
     digitalWrite(ledPin, HIGH); // Turn on LED
     MyBTModule.println("LED is turned on\n"); // Then send status message to Android
     msg = ""; // reset command
-  } else {
-    if (msg == "<turn off>"){
-      digitalWrite(ledPin, LOW); // Turn off LED
-      MyBTModule.println("LED is turned off\n"); // Then send status message to Android
-      msg = ""; // reset command
-    }
   }
+
+  if (msg == "<turn off>") {
+    digitalWrite(ledPin, LOW); // Turn off LED
+    MyBTModule.println("LED is turned off\n"); // Then send status message to Android
+    msg = ""; // reset command
+  }
+  if (msg == "<down>") {
+    MyBTModule.println("recieved down\n"); // Then send status message to Android
+    msg = ""; // reset command
+  }
+  if (msg == "<left>") {
+    MyBTModule.println("recieved left\n"); // Then send status message to Android
+    msg = ""; // reset command
+  }
+  if (msg == "<right>") {
+    MyBTModule.println("recieved right\n"); // Then send status message to Android
+    msg = ""; // reset command
+  }
+
+
 }
