3b3ecda3981b0dc555efdaf7790b1682569ffb99
TheHackGuy <thehackguy@icloud.com>
2022-01-11 09:24:39 +0100

    Create Change_name.ino

---
+++ ArduinoCode/Change_name/Change_name.ino
@@ -0,0 +1,21 @@
+#include <SoftwareSerial.h> 
+SoftwareSerial MyBTModule(3, 2); // RX & TX pins 
+String msg;
+
+void setup() 
+{   
+ Serial.begin(38400); 
+ MyBTModule.begin(38400); 
+} 
+
+
+void loop() 
+{ 
+ if (MyBTModule.available() > 0) {
+   msg = MyBTModule.readString();
+   Serial.println("BT module send: " + msg);
+ }
+ 
+ if (Serial.available()) 
+     MyBTModule.write(Serial.read());
+}  
