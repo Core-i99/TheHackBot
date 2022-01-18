dbd2562a390f3225a9a396e95026fd43652245f6
TheHackGuy <thehackguy@icloud.com>
2022-01-18 09:20:47 +0100

    improve comments

--- ArduinoCode/TheHackBot_Control/TheHackBot_Control.ino
+++ ArduinoCode/TheHackBot_Control/TheHackBot_Control.ino
@@ -6,7 +6,7 @@
 #include <SoftwareSerial.h>
 SoftwareSerial BTModule(3, 2); // RX & TX pins
 
-const int ledUP = 13; // Built in LED in Arduino board
+const int ledUP = 13;
 const int ledLEFT = 12;
 const int ledRIGHT = 10;
 const int ledDOWN = 11;
@@ -19,8 +19,8 @@ void setup() {
   pinMode(ledLEFT, OUTPUT);
   pinMode(ledRIGHT, OUTPUT);
   pinMode(ledDOWN, OUTPUT);
-  BTModule.begin(9600);
-  Serial.begin(9600); // Communication rate of the Bluetooth Module
+  BTModule.begin(9600); // Communication rate of the Bluetooth Module
+  Serial.begin(9600); 
 }
 
 void loop() {
@@ -29,27 +29,27 @@ void loop() {
     msg = BTModule.read(); // Read the message
     switch (msg) {
       case '1':
-        digitalWrite(ledUP, 1); // Turn on LED
-        BTModule.println("LEDUP is turned on\n"); // Then send status message to Android
+        digitalWrite(ledUP, 1); // Turn on LEDUP
+        BTModule.println("LEDUP is turned on\n"); // Send status message to Android
         break;
       case '2':
-        digitalWrite(ledLEFT, 1); // Turn on LED
-        BTModule.println("LEDLEFT is turned on\n"); // Then send status message to Android
+        digitalWrite(ledLEFT, 1); // Turn on LEDLEFT
+        BTModule.println("LEDLEFT is turned on\n"); // Send status message to Android
         break;
       case '3':
-        digitalWrite(ledRIGHT, 1); // Turn on LED
-        BTModule.println("LEDRIGHT is turned on\n"); // Then send status message to Android
+        digitalWrite(ledRIGHT, 1); // Turn on LEDRIGHT
+        BTModule.println("LEDRIGHT is turned on\n"); // Send status message to Android
         break;
       case '4':
-        digitalWrite(ledDOWN, 1); // Turn on LED
-        BTModule.println("LEDDOWN is turned on\n"); // Then send status message to Android
+        digitalWrite(ledDOWN, 1); // Turn on LEDDOWN
+        BTModule.println("LEDDOWN is turned on\n"); // Send status message to Android
         break;
       case '5':
-        digitalWrite(ledUP, 0); // Turn off LED
-        digitalWrite(ledLEFT, 0); // Turn off LED
-        digitalWrite(ledRIGHT, 0); // Turn off LED
-        digitalWrite(ledDOWN, 0); // Turn off LED
-        BTModule.println("LED's are turned off\n"); // Then send status message to Android
+        digitalWrite(ledUP, 0); // Turn off LEDUP
+        digitalWrite(ledLEFT, 0); // Turn off LEDLEFT
+        digitalWrite(ledRIGHT, 0); // Turn off LEDRIGHT
+        digitalWrite(ledDOWN, 0); // Turn off LEDDOWN
+        BTModule.println("LED's are turned off\n"); // Send status message to Android
         break;
      default:
         BTModule.println("That's not an option!");
