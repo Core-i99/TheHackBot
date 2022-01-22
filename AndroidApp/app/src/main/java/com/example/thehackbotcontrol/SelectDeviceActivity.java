076156c63ea76c4ee20aac54eb56e0e99994793b
TheHackGuy <thehackguy@icloud.com>
2022-01-22 20:53:09 +0100

    Better way of listing the paired devices

--- AndroidApp/app/src/main/java/com/example/thehackbotcontrol/SelectDeviceActivity.java
+++ AndroidApp/app/src/main/java/com/example/thehackbotcontrol/SelectDeviceActivity.java
@@ -1,58 +1,58 @@
 package com.example.thehackbotcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.DefaultItemAnimator;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
-
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 public class SelectDeviceActivity extends AppCompatActivity {
+    ListView listView;
+    BluetoothAdapter myBluetoothAdapter;
+    public static String DEVICE_ADDRESS;
+    public static String DEVICE_NAME = "TheHackBot"; //temporarily way to set device name
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_device);
         ImageButton back = (ImageButton) findViewById(R.id.Back2);
         back.setOnClickListener(v -> finish());
+        listView=(ListView) findViewById(R.id.DeviceListPaired);
 
-        // Bluetooth Setup
-        BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
 
         // Get List of Paired Bluetooth Device
-        Set<BluetoothDevice> pairedDevices = bluetoothAdapter.getBondedDevices();
-        List<Object> deviceList = new ArrayList<>();
-        if (pairedDevices.size() > 0) {
-            // There are paired devices. Get the name and address of each paired device.
-            for (BluetoothDevice device : pairedDevices) {
-                String deviceName = device.getName();
-                String deviceHardwareAddress = device.getAddress(); // MAC address
-                DeviceInfoModel deviceInfoModel = new DeviceInfoModel(deviceName,deviceHardwareAddress);
-                deviceList.add(deviceInfoModel);
-            }
-            // Display paired device using recyclerView
-            RecyclerView recyclerView = findViewById(R.id.recyclerViewDevice);
-            recyclerView.setLayoutManager(new LinearLayoutManager(this));
-            DeviceListAdapter deviceListAdapter = new DeviceListAdapter(this,deviceList);
-            recyclerView.setAdapter(deviceListAdapter);
-            recyclerView.setItemAnimator(new DefaultItemAnimator());
-        } else {
-            View view = findViewById(R.id.recyclerViewDevice);
-            Snackbar snackbar = Snackbar.make(view, "Activate Bluetooth or pair a Bluetooth device", Snackbar.LENGTH_INDEFINITE);
-            snackbar.setAction("OK", view1 -> finish());
-            snackbar.show();
-        }
+        Set<BluetoothDevice> PairedDevices=myBluetoothAdapter.getBondedDevices();
+        String[] strings=new String[PairedDevices.size()];
+        int index=0;
 
+        if (PairedDevices.size()>0){
+            for(BluetoothDevice device:PairedDevices) {
+                strings[index]=device.getAddress();
+                index++;
+            }
+            ArrayAdapter<String> arrayAdapter= new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_1, strings);
+            listView.setAdapter(arrayAdapter);
+
+            listView.setOnItemClickListener((parent, view, position, id) -> {
+                DEVICE_ADDRESS = strings[position];
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                intent.putExtra("deviceName", DEVICE_NAME);
+                Log.e("STAT", "Send device name to Main Activity: " + DEVICE_NAME);
+                intent.putExtra("deviceAddress", DEVICE_ADDRESS);
+                Log.e("STAT", "Send device address to Main Activity: " + DEVICE_ADDRESS);
+                startActivity(intent);
+            });
+        } else {
+            Toast.makeText(getApplicationContext(), "Activate Bluetooth or pair a Bluetooth device", Toast.LENGTH_SHORT).show();
+        }
     }
 }
