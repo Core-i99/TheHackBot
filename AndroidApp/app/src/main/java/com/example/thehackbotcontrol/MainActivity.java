50ef9a05a8b65c28d132f25457f92d4643c7aa84
TheHackGuy <thehackguy@icloud.com>
2022-02-08 09:01:09 +0100

    Add LICENSE headers

--- AndroidApp/app/src/main/java/com/example/thehackbotcontrol/MainActivity.java
+++ AndroidApp/app/src/main/java/com/example/thehackbotcontrol/MainActivity.java
@@ -1,7 +1,20 @@
 /*
-HC05 - Bluetooth module
-© Stijn Rombouts - 2021
-*/
+ * HC05 - Bluetooth module - MainActivity
+ * Copyright (C) 2022 Stijn Rombouts
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.example.thehackbotcontrol;
 
 import androidx.appcompat.app.AppCompatActivity;
