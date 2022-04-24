297f95fe5dde5fbcca26df14c3287bc60a2da9a6
Core i99 <thehackguy@icloud.com>
2022-04-24 14:35:02 +0200

    fix end of comment

--- ArduinoCode/TheHackBot_Control/TheHackBot_Control.ino
+++ ArduinoCode/TheHackBot_Control/TheHackBot_Control.ino
@@ -14,7 +14,7 @@
  *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
 #include <SoftwareSerial.h>
 SoftwareSerial BTModule(3, 2); // RX & TX pins
